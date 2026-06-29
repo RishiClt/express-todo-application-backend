3c4e7d6586def2c8ffdeaa8c89941c0f25c516ec
Rishi <rishikeshcalicut123@gmail.com>
2026-06-29 19:51:10 +0530

    re-written the code , added persistent and consistent id population to the application , added add , get , getById, delete and modify functionalities

--- index.js
+++ index.js
@@ -1,63 +1,105 @@
+//creating an express server as backend for a todo-application
 
 const express = require("express");
 const fs = require("fs").promises;
 const app = express();
 
-let id =0;
+
+
 async function readFileFromDB(){
     try{
-        const dataFromTextFile = await fs.readFile('a.txt','utf-8');
-        return JSON.parse(dataFromTextFile);
+        const dataFrom = await fs.readFile('a.txt','utf-8');
+        return JSON.parse(dataFrom);
     }catch(err){
         return [];
     }
 }
 
-async function writeFileToDB(task){
-    await fs.writeFile('a.txt',JSON.stringify(task , null , 2),'utf-8');
+async function writeDataToDB(task){
+    await fs.writeFile('a.txt',JSON.stringify(task,null , 2),'utf-8');
+}
+
+async function setID(){
+    let arrayFromDB =await readFileFromDB()
+    return arrayFromDB.length -1;
 }
 
 
 app.use(express.json());
-
 app.post("/add", async(req , res)=>{
-    const preparedData = await readFileFromDB();
-    const inputMessage = req.body.message;
-    const data = {
+    let datainTextFile = await readFileFromDB();
+    let id = await setID();
+    const obj = {
         id : ++id,
-        message : inputMessage
+        message : req.body.message
     }
-
-    console.log("Data before pushing "+preparedData);
-    preparedData.push(data);
-    console.log("Data after pushing "+preparedData);
-    await writeFileToDB(preparedData);
+    datainTextFile.push(obj);
+    await writeDataToDB(datainTextFile);
     res.status(200).json({
-        message : "Data added successfully."
+        message :"Data wtitten to the file successfully"
     })
+})
 
+app.delete("/delete/:userID" ,async(req , res)=>{
+    let count =0;
+    let finalCount =0
+    let datainTextFile = await readFileFromDB();
+    // console.log(datainTextFile);
+    datainTextFile.forEach((element)=>{
+        if(element.id === parseInt(req.params.userID)){
+            finalCount = count;
+        }else{
+            count++;
+        }
+    })
+    let indexOfTarget = finalCount;
+    datainTextFile.splice(indexOfTarget,1);
+    await writeDataToDB(datainTextFile);
+    res.status(200).json({
+        message : "Deleted data from file successfully"
+    })
+})
+
+app.put("/modify/:userID" , async(req , res)=>{
+    let count = 0;
+    let finalCount = 0
+    let datainTextFile = await readFileFromDB();
+    datainTextFile.forEach((element)=>{
+        if(element.id === parseInt(req.params.userID)){
+            finalCount = count;
+        }else{
+            count++;
+        }
+    })
+    let indexOfTarget = finalCount;
+    // console.log(indexOfTarget);
+    // console.log(datainTextFile);
+    let newMessage = req.body.message;
+    datainTextFile[indexOfTarget].message = newMessage;
+    await writeDataToDB(datainTextFile);
+    res.status(200).json({
+        message : "todo modified"
+    })
 })
 
 
-app.get("/:taskid",async(req , res)=>{
-    let taskID = parseInt(req.params.taskid);
-    console.log(taskID);
-    const dataInDB = await readFileFromDB();
-    let messageStored = dataInDB.filter(element => element.id === taskID);
-    console.log(messageStored);
-    res.json({
-        todo : messageStored[0].message
-    })
-
+app.get("/getAll", async(req , res)=>{
+    let datainTextFile = await readFileFromDB();
+    res.status(200).json(datainTextFile);
 })
 
+app.get("/get/:id" , async(req, res)=>{
+    let datainTextFile = await readFileFromDB();
+    let obj = {};
+    datainTextFile.forEach(element =>{
+        if(element.id === parseInt(req.params.id)){
+            obj = element;
+        }
+    })
 
-app.put("/update/:taskID", async(req, res)=>{
-    let taskID = parseInt(req.params.taskID);
-    const dataInDB = await readFileFromDB();
-    
+    res.status(200).json(obj);
 })
 
 app.listen(3000,()=>{
-    console.log("Application started listening on port 3000");
-});
+    console.log("Application started listening on port 3000")
+})
